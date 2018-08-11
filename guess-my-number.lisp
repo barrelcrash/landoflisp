8058ed6bc31ee6e877081a4cef0bc70d057dce66
Luke Abel <luke.abel@gmail.com>
2018-08-11 12:20:39 -0400

    ch2 - local variables and functions

--- guess-my-number.lisp
+++ guess-my-number.lisp
@@ -1,6 +1,5 @@
 ;; the computer guesses your number between 1 and 100 (binary search)
 
-;; written with global variables
 (defparameter *small* 1)
 
 (defparameter *big* 100)
@@ -21,5 +20,28 @@
   (defparameter *big* 100)
   (guess-my-number))
 
-;; local variables
+;; local variables and functions
 
+(defun local-var ()
+  (let ((a 5)
+	(b 6))
+    (+ a b))) ;; 11
+
+(defun local-func ()
+  (flet ((f (n)
+	   (+ n 10)))
+    (f 5))) ;; 15
+
+(defun multi-local-func ()
+  (flet ((f (n)
+	   (+ n 10))
+	 (g (n)
+	   (- n 3)))
+    (g (f 5)))) ;; 12
+
+(defun labels-func ()
+  (labels ((a (n)
+	     (+ n 5))
+	   (b (n)
+	     (+ (a n) 6)))
+    (b 10))) ;; 21
