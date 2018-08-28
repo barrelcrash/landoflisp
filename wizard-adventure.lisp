7506718fe8605495d97394e89fbaececa58c14eb
Luke Abel <luke.abel@gmail.com>
2018-08-28 17:01:46 -0400

    Update wizard-adventure.lisp

--- wizard-adventure.lisp
+++ wizard-adventure.lisp
@@ -5,6 +5,11 @@
                         (garden (you are in a beautiful garden. there is a well in front of you.))
                         (attic (you are in the attic. there is a giant welding torch in the corner))))
 
+(defparameter *edges* '((living-room (garden west door)
+                                     (attic upstairs ladder))
+                        (garden (living-room east door))
+                        (attic (living-room downstairs ladder))))
+
 (defun describe-location (location nodes)
     (cadr (assoc location nodes)))
 
